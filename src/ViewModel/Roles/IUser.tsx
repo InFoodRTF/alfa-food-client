a34fb4e407beab75f46d91b930ff1f845b0366c7
Devilboi99 <turnickii.n@gmail.com>
2023-01-27 10:05:24 +0500

    логика в UserStore

--- src/ViewModel/Roles/IUser.tsx
+++ src/ViewModel/Roles/IUser.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Profile from "../../Entity/Profile";
 import Role from "../Enum/Role";
+import IToken from "./IToken";
 interface IUser{
     username: string;
     first_name: string;
     last_name: string;
     profile: Profile
     role: Role
+    GetUser(Token: IToken): void
 }
 
 export default IUser;
