9b762465e88a56efc863476858fd662a9507a0b4
Devilboi99 <turnickii.n@gmail.com>
2023-05-28 16:03:51 +0500

    добавил адресс

--- src/Pages/HttpPages.tsx
+++ src/Pages/HttpPages.tsx
@@ -1,13 +1,14 @@
+const baseUrl = "/alfa-food-client"
 class HttpPages {
-    Profile: string = "/Profile"
-    Orders: string = "/Orders"
-    Login: string = "/login"
-    Auth: string = "*"
-    NotFound: string = "/NotPages";
-    Products: string = '/Products'
-    MyClass: string = "/myClasses"
-    UploadData: string = "/UploadData"
-    CreateMenu: string = "Menus"
+    Profile: string = baseUrl + "/Profile"
+    Orders: string = baseUrl + "/Orders"
+    Login: string = baseUrl +  "/login"
+    Auth: string = baseUrl + "*"
+    NotFound: string = baseUrl + "/NotPages";
+    Products: string = baseUrl + '/Products'
+    MyClass: string = baseUrl + "/myClasses"
+    UploadData: string = baseUrl + "/UploadData"
+    CreateMenu: string = baseUrl +  "Menus"
 }
 
 export default new HttpPages();
