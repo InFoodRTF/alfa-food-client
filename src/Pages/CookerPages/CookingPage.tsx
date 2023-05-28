fadd819a834c203d7b01c8b5c3c11849b58921d6
Devilboi99 <turnickii.n@gmail.com>
2023-05-28 12:07:58 +0500

    Рефакторинг, удаление мусора

---
+++ src/Pages/CookerPages/CookingPage.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import buttonStyles from "../Components/BaseButton/BaseButItem.module.css";
+/*export class CookingPage extends React.Component{
+    render() {
+        return (
+             <body style={{background:"#F8F8F8"}}>
+            <div style={{
+                display: "flex",
+                flexDirection: "row",
+                gap: "20px",
+                width: "1119px",
+                marginTop: "26px",
+                marginLeft: "auto",
+                marginRight: "auto",
+                paddingBottom: "60px",
+                minHeight: "580px"
+            }}>
+                <div style={{display: "flex", flexDirection: "column", gap: "39px"}}>
+                    <Calendar/>
+                    <div style={{display: "flex", flexDirection: "column", gap: "26px", paddingTop: "2.5px"}}>
+                        <BaseButItem w={264} h={44} style={buttonStyles.cardButton} text={"Добавить новое меню"}/>
+                        <LeftMenuCooking/>
+                        <BaseButItem w={264} h={44} style={buttonStyles.cardButton} text={"Изменить"}/>
+                    </div>
+                </div>
+                <div style={{display: "flex", flexDirection: "column", gap: "23px"}}>
+                    <div style={{display: "flex", flexDirection: "column", gap: "18px"}}>
+                        <InputCook/>
+                        <div style={{display: "flex", flexDirection: "row", gap: "20px"}}>
+                            <FilterFoodItem/>
+                            <BaseButItem w={208} h={44} style={buttonStyles.cardButton} text={"Добавить категорию"}/>
+                        </div>
+                    </div>
+                    <div style={{display: "flex", flexDirection: "row", gap: "20px", paddingTop: "2.5px"}}>
+                        <CreatingCard/>
+                        <EditingCard/>
+                        <EditingCard/>
+                    </div>
+                </div>
+            </div>
+            <FooterCooking firstButtonText={"Составить меню"} secondButtonText={"Добавить блюдо"} thirdButtonText={"Выгрузить отчет"}/>
+            </body>
+        )
+    }
+}*/
---
+++ src/Pages/Login/LoginPage.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import styles from "./LoginPage.module.css"
+import logo from './logo.svg'
+import {Button, Form} from "react-bootstrap";
+import {inject, observer} from "mobx-react";
+import AuthStore from "./AuthStore";
+import statusResponse from "../../Api/StatusResponse";
+import httpPages from "../HttpPages";
+import {Navigate} from "react-router-dom";
+import StatusResponse from "../../Api/StatusResponse";
+
+
+type props = {
+    authStore: AuthStore;
+}
+
+@inject("authStore")
+@observer
+export class LoginPage extends React.Component {
+    get injected(): props {
+        return this.props as props;
+    }
+
+    CheckResponse(status: statusResponse) { // todo подумать над этой штукой, она не логиная
+        switch (status) {
+            case StatusResponse.Ok:
+                return <Navigate to={httpPages.Profile}/>
+            case StatusResponse.BadRequest:
+                return <span>Не корректный пароль или логин</span>
+            case StatusResponse.ServerNotFound:
+                return <span>Мы не работает</span>
+            case StatusResponse.NotServer:
+                break;
+            default:
+                return <></>
+        }
+    }
+
+    render() {
+        let {authStore} = this.injected;
+        return (
+            <div className="m-5">
+                <Container className={styles.OrderHistoryContainer}>
+                    <Card className={styles.OrderHistoryCard}>
+                        <div className="">
+                            <div className="text-center m-3 p-3">
+                                <img src={logo} alt="Logo"/>
+                            </div>
+                            <h2 className="fw-bold m-3 p-3 text-center">Вход</h2>
+
+                            <div className="row d-flex justify-content-center">
+                                <Form.Group className="mb-3 w-75" controlId="formBasicPassword">
+                                    <Form.Control type="text" placeholder="Логин"
+                                                  onChange={v => authStore.User.ChangeUserName(v.target.value)}/>
+                                </Form.Group>
+                            </div>
+
+                            <div className="row d-flex justify-content-center">
+                                <Form.Group className="mb-3 w-75" controlId="formBasicPassword">
+                                    <Form.Control type="password" placeholder="Пароль"
+                                                  onChange={v => authStore.User.ChangePassword(v.target.value)}/>
+                                </Form.Group>
+                            </div>
+
+                            <div className="text-center m-3">Забыли пароль?</div>
+
+                            <div className="text-center">
+                                <Button type={"button"} className="mb-5 w-50 btn btn-danger"
+                                        onClick={() => authStore.UserAuth()}>Войти</Button>
+
+                            </div>
+                            {this.CheckResponse(authStore.ResponseStatus)}
+                        </div>
+
+                    </Card>
+                </Container>
+
+            </div>
+
+
+        )
+    }
+}
