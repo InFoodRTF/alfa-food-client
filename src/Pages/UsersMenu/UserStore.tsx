3e9dc1b70b4112d1cc235705e0cd16dbcd7a7db6
Vadim Romanov <58142149+GamerZombar@users.noreply.github.com>
2023-05-29 19:36:51 +0500

    Update UserStore.tsx

--- src/Pages/UsersMenu/UserStore.tsx
+++ src/Pages/UsersMenu/UserStore.tsx
@@ -3,6 +3,7 @@ import NotAuthUser from "../../Model/Role/NotAuthUser";
 import IUser from "../../Model/Interface/IUser";
 import baseStoreToken from "../../Api/StoreAdapterApi";
 import StoreAdapterApi from "../../Api/StoreAdapterApi";
+import Requests from "../../Api/Requests";
 
 class UserStore extends StoreAdapterApi {
     @observable
@@ -17,7 +18,7 @@ class UserStore extends StoreAdapterApi {
 
     @action
     async AuthByToken() {
-        this.User = await this.getDataByToken(this.urlGetUser);
+        this.User = await this.getDataByToken(Requests.User);
     }
 
     public ChangeLoad(){
