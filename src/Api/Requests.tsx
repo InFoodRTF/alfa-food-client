87f1c83a289705e25a222a2329aa91b0e8918213
Vadim Romanov <58142149+GamerZombar@users.noreply.github.com>
2023-05-29 19:07:06 +0500

    Add /api to the beginning of each request

--- src/Api/Requests.tsx
+++ src/Api/Requests.tsx
@@ -1,16 +1,16 @@
 class Requests { // ухх, это выглядит все сложнее и сложнее)))
-    GetTokenByUser = "/auth/login/";
-    User = "/user/";
-    Orders = "/orders/";
-    GetStudets = "/students/";
+    GetTokenByUser = "/api/auth/login/";
+    User = "/api/user/";
+    Orders = "/api/orders/";
+    GetStudets = "/api/students/";
     FoodCards = "/Food";
-    GetCart = `/cart/`;
-    AddProductInCart = `/cart/add/`;
-    RemoveProductFromCart = `/cart/remove/`;
-    GetGrades = `/grades/`;
-    GetMenu = (date: string) => `/menu/?date=${date}`
-    SwitchCart = (studentId: number, date: string) => `/cart?menu_date=${date}&student_id=${studentId}`;
-    GetAttendancesStudent = (gradeName: string, mealCategory: string, date: string) => `/attendances/?grade=${gradeName}&meal_category=${mealCategory}&date=${date}`
+    GetCart = `/api/cart/`;
+    AddProductInCart = `/api/cart/add/`;
+    RemoveProductFromCart = `/api/cart/remove/`;
+    GetGrades = `/api/grades/`;
+    GetMenu = (date: string) => `/api/menu/?date=${date}`
+    SwitchCart = (studentId: number, date: string) => `/api/cart?menu_date=${date}&student_id=${studentId}`;
+    GetAttendancesStudent = (gradeName: string, mealCategory: string, date: string) => `/api/attendances/?grade=${gradeName}&meal_category=${mealCategory}&date=${date}`
 }
 
 export default new Requests();
