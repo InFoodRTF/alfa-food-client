024047ef5058501100a6b63437de285088ec4285
Vadim Romanov <58142149+GamerZombar@users.noreply.github.com>
2023-05-29 19:59:36 +0500

    Update StudentsStore.tsx

--- src/Pages/ParentPages/ParentProfile/Store/StudentsStore.tsx
+++ src/Pages/ParentPages/ParentProfile/Store/StudentsStore.tsx
@@ -3,7 +3,7 @@ import {action, makeObservable, observable} from "mobx";
 import Grade from "./Grade";
 import Toggle from "../../../../Model/Toggle";
 import {IStudent} from "./IStudent";
-import Requests from "../../../Api/Requests";
+import Requests from "../../Api/Requests";
 
 class StudentsStore extends StoreAdapterApi {
     @observable
@@ -26,7 +26,7 @@ class StudentsStore extends StoreAdapterApi {
 
     @action
     async LoadInfoGrade(name: string): Promise<void> {
-        this.Grades[name] = await this.getDataByToken<Grade>(`/grades/${name}`);
+        this.Grades[name] = await this.getDataByToken<Grade>(Requests.GetGrades + `${name}`);
         this.Toggle[name] = new Toggle();  // официально - это самый лютый кринж который я делал, название функций делает ваще другое все каой тугл зочем он здесь? ватт?
         console.log(`${this.Grades[name].name}`)
     }
