b2621fc1e5e163796d9cda492271b3c13aee6be3
Devilboi99 <turnickii.n@gmail.com>
2023-06-15 09:27:08 +0500

    hotfix 8

--- src/componets/ChooseProductAdd/ChooseProductAdd.tsx
+++ src/componets/ChooseProductAdd/ChooseProductAdd.tsx
@@ -23,7 +23,7 @@ export class ChooseProductAdd extends React.Component<props>{
                 minHeight: "620px"
             }}>
                 <SearchCard/>
-                {products.map((p) => <CookingFoodCard product={p} onClick={() => this.props.onClick(p)} />)}
+                {Array.isArray(products) && products.map((p) => <CookingFoodCard product={p} onClick={() => this.props.onClick(p)} />)}
             </div>
         )
     }
